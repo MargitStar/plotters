ca9a7f8fe512ea7aacbcc7a5615ada127ba9575f
margitstar <sovenokbymargo@mail.ru>
2021-03-10 20:48:50 +0300

    Add GET and POST for all users

--- plotters/plotter/views.py
+++ plotters/plotter/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from datetime import datetime
+from rest_framework.permissions import IsAuthenticated
+from django.db.utils import IntegrityError
 
-# Create your views here.
+from .models import Plotter, User
+from .serializers import PlotterSerializer
+
+
+class PlotterView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        snippets = Plotter.objects.filter().all()
+        serializer = PlotterSerializer(snippets, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def post(self, request):
+        user = request.POST.get('user')
+        serializer = PlotterSerializer(data=request.data)
+        try:
+            if serializer.is_valid(raise_exception=True):
+                order_saved = serializer.save()
+                return Response({"success": order_saved.pk})
+        except IntegrityError:
+            return Response("This Plotter already exists!")
