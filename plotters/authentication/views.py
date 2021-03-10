b7c78822b30871e2de0437d2159a01500d4b1276
margitstar <sovenokbymargo@mail.ru>
2021-03-10 16:48:41 +0300

    And register and user views

--- plotters/authentication/views.py
+++ plotters/authentication/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, mixins, status
+from rest_framework.response import Response
+from .serializers import RegisterSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from django.contrib.auth.models import Group
 
-# Create your views here.
+
+class RegisterApi(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        group, created = Group.objects.get_or_create(name="Customer")
+        group.user_set.add(user)
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "message": "User Created Successfully.  Now perform Login to get your token",
+        })
+
+
+class UserApi(APIView):
+
+    def get(self, request, *args, **kwargs):
+        snippets = User.objects.filter(pk=request.user.pk).all()
+        serializer = UserSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        user = User.objects.filter(pk=request.user.pk).first()
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
