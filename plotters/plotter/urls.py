faba72f04e42416801f27529e925bc8332540f02
margitstar <sovenokbymargo@mail.ru>
2021-03-11 00:19:00 +0300

    Add Detail view url

--- plotters/plotter/urls.py
+++ plotters/plotter/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import PlotterView
+from .views import PlotterView, PlotterDetailView
 
 app_name = 'plotters'
 urlpatterns = [
     path('', PlotterView.as_view()),
+    path('<int:pk>/', PlotterDetailView.as_view())
 ]
