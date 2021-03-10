d026e5c5b809865b84a82aae5035fc94f18722d0
margitstar <sovenokbymargo@mail.ru>
2021-03-10 16:47:50 +0300

    Add urls for registration, login, tokens

---
+++ plotters/authentication/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterApi, UserApi
+
+app_name = 'authentication'
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterApi.as_view(), name='register'),
+    path('crud/', UserApi.as_view(), name='user')
+]
