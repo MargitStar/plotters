8f2c824d6583721021a7f1a014fc3805f94af977
margitstar <sovenokbymargo@mail.ru>
2021-03-11 21:02:00 +0300

    Add post and put method to Mold

--- plotters/mold/serializers.py
+++ plotters/mold/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
+from .models import Mold
 
 
 class MoldSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=150)
+
+    def create(self, validated_data):
+        return Mold.objects.create(**validated_data)
--- plotters/mold/views.py
+++ plotters/mold/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from django.db.utils import IntegrityError
+from django.http import Http404
 
-# Create your views here.
+from .models import Mold
+from .serializers import MoldSerializer
+
+
+class MoldView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        snippets = Mold.objects.filter().all()
+        serializer = MoldSerializer(snippets, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def post(self, request):
+        user = self.request.user
+        if user.is_superuser:
+            serializer = MoldSerializer(data=request.data)
+            try:
+                if serializer.is_valid(raise_exception=True):
+                    order_saved = serializer.save()
+                    return Response({"success": order_saved.pk})
+            except IntegrityError:
+                return Response("This Mold already exists!")
