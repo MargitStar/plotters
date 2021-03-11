1f8d447a1fb3394adf72f2dbb082611846c39101
margitstar <sovenokbymargo@mail.ru>
2021-03-11 13:12:12 +0300

    Add url for users detail view

--- plotters/authentication/urls.py
+++ plotters/authentication/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterApi, UserApi
+from .views import RegisterApi, UserApi, UserDetailView
 
 app_name = 'authentication'
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterApi.as_view(), name='register'),
-    path('crud/', UserApi.as_view(), name='user')
+    path('crud/', UserApi.as_view(), name='users'),
+    path('user/<int:pk>/', UserDetailView.as_view(), name="user")
 ]
