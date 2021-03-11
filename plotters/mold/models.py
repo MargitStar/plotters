da6cb90d5fa4b168832cd21447ec0c1b3d571fcc
margitstar <sovenokbymargo@mail.ru>
2021-03-11 16:15:38 +0300

    Add mold model

--- plotters/mold/models.py
+++ plotters/mold/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Mold(models.Model):
+    name = models.CharField(
+        'Name',
+        max_length=150,
+        unique=True
+    )
+
+    def __str__(self):
+        return f"Mold f{self.name}"
