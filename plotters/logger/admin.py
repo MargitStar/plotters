34cce7ef6edec8b1730f15ea96e2dcbc9b4c5768
margitstar <sovenokbymargo@mail.ru>
2021-03-12 19:35:48 +0300

    Add MoldStatistics model

--- plotters/logger/admin.py
+++ plotters/logger/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Cutout, CutoutAdmin
+from .models import Cutout, CutoutAdmin, MoldStatistics
 
 admin.site.register(Cutout, CutoutAdmin)
+admin.site.register(MoldStatistics)
