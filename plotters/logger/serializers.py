c67d152a5626a41f259a0518b9f35313a8577135
margitstar <sovenokbymargo@mail.ru>
2021-03-12 18:24:27 +0300

    Add post method for Cutout

--- plotters/logger/serializers.py
+++ plotters/logger/serializers.py
@@ -2,16 +2,20 @@ from rest_framework import serializers
 from .models import Cutout
 
 
-class CutoutSerializer(serializers.Serializer):
-    user_id = serializers.IntegerField()
+
+class CutoutGetSerializer(serializers.Serializer):
     plotter_id = serializers.IntegerField()
     mold_id = serializers.IntegerField()
     created_date = serializers.DateTimeField(format="%d %b, %Y - %Ih %Mm %S %p")
 
+
+class CutoutPostSerializer(serializers.Serializer):
+    plotter_id = serializers.IntegerField()
+    mold_id = serializers.IntegerField()
+
     def create(self, validated_data):
-        user_id = validated_data.get('user_id')
-        plotter_id = validated_data.get('plotter_id')
-        mold_id = validated_data.get('mold_id')
-        created_date = validated_data.get('created_date')
+        plotter_id = serializers.IntegerField()
+        mold_id = serializers.IntegerField()
+
         cutout = Cutout.objects.create(**validated_data)
         return cutout
--- plotters/logger/views.py
+++ plotters/logger/views.py
@@ -3,8 +3,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Cutout
 from datetime import datetime
-from .serializers import CutoutSerializer
+from .serializers import CutoutPostSerializer, CutoutGetSerializer
 from rest_framework.permissions import IsAuthenticated
+from django.db.utils import IntegrityError
 
 
 class CutoutView(APIView):
@@ -14,11 +15,24 @@ class CutoutView(APIView):
         user = request.user
         if user.is_superuser:
             snippets = Cutout.objects.all()
-            serializer = CutoutSerializer(snippets, many=True, context={'request': request})
+            serializer = CutoutGetSerializer(snippets, many=True, context={'request': request})
         elif user.groups.filter(name='Dealer').exists():
             pass
         else:
             snippets = Cutout.objects.filter(user=request.user).all()
-            serializer = CutoutSerializer(snippets, many=True, context={'request': request})
+            serializer = CutoutGetSerializer(snippets, many=True, context={'request': request})
         return Response(serializer.data)
 
+    def post(self, request):
+        user = request.user
+        try:
+            if user.groups.filter(name='Customer').exists():
+                try:
+                    serializer = CutoutPostSerializer(data=request.data)
+                    if serializer.is_valid(raise_exception=True):
+                        cutout = serializer.save(user=self.request.user, created_date=datetime.now())
+                        return Response({"success": cutout.pk})
+                except IntegrityError:
+                    return Response("This plotter or mold does not exist")
+        except PermissionError:
+            return Response("User has no permission")
