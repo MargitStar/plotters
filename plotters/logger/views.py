1333a5a2bf38fd4cb0f0560db0f726eff7def2e5
margitstar <sovenokbymargo@mail.ru>
2021-03-12 17:46:50 +0300

    Add get for Cutout

--- plotters/logger/views.py
+++ plotters/logger/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Cutout
+from datetime import datetime
+from .serializers import CutoutSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class CutoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        user = request.user
+        if user.is_superuser:
+            snippets = Cutout.objects.all()
+            serializer = CutoutSerializer(snippets, many=True, context={'request': request})
+        elif user.groups.filter(name='Dealer').exists():
+            pass
+        else:
+            snippets = Cutout.objects.filter(user=request.user).all()
+            serializer = CutoutSerializer(snippets, many=True, context={'request': request})
+        return Response(serializer.data)
+
