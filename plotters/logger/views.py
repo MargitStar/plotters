c216d1a902f7f682613fcb03dac394658fbb6054
margitstar <sovenokbymargo@mail.ru>
2021-03-14 18:56:08 +0300

    Add last print to plotter statistics

--- plotters/logger/views.py
+++ plotters/logger/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -39,13 +39,15 @@ class CutoutView(APIView):
                     return Response("User has no permission!")
                 if serializer.is_valid(raise_exception=True) and mold_serializer.is_valid(
                         raise_exception=True) and plotter_serializer.is_valid(raise_exception=True):
-                    if plotter.cutouts >= request.data['amount']:
+                    if plotter.cutouts >= request.data['amount'] != 0:
                         cutout = serializer.save(user=self.request.user, created_date=datetime.now())
                         mold = mold_serializer.save()
-                        plotter_statistics = plotter_serializer.save(ip=plotter.ip)
+                        plotter_statistics = plotter_serializer.save(ip=plotter.ip, last_cutout_date=date.today())
                         plotter.cutouts -= request.data['amount']
                         plotter.save()
                         return Response({"success": cutout.pk})
+                    elif request.data['amount'] is 0:
+                        return Response('Please choose another amount, not 0!')
                     else:
                         return Response(f'You can cut out only {plotter.cutouts}')
             except IntegrityError:
